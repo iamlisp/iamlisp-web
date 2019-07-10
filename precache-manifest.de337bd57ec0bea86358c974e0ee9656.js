792075bf6ecfc58e418ad3275954eb88c4147cc0
Roman Lakhtadyr <roman.lakhtadyr@gmail.com>
2019-07-10 21:50:37 +0300

    Updates

---
+++ precache-manifest.de337bd57ec0bea86358c974e0ee9656.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "63d205d40df611c9a798f7b31055a6ae",
+    "url": "/iamlisp-web/index.html"
+  },
+  {
+    "revision": "d0e53eeef8072bab6fe9",
+    "url": "/iamlisp-web/static/css/2.fd1bd5f1.chunk.css"
+  },
+  {
+    "revision": "09ac9f4966025d066c16",
+    "url": "/iamlisp-web/static/css/main.4ebf4eae.chunk.css"
+  },
+  {
+    "revision": "d0e53eeef8072bab6fe9",
+    "url": "/iamlisp-web/static/js/2.a47161a6.chunk.js"
+  },
+  {
+    "revision": "09ac9f4966025d066c16",
+    "url": "/iamlisp-web/static/js/main.c5df0575.chunk.js"
+  },
+  {
+    "revision": "4834ad6160723c288421",
+    "url": "/iamlisp-web/static/js/runtime~main.f16edc94.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/iamlisp-web/precache-manifest.a477ce8304c4b336ef267982ea01a246.js"
+  "/iamlisp-web/precache-manifest.de337bd57ec0bea86358c974e0ee9656.js"
 );
 
 self.addEventListener('message', (event) => {
